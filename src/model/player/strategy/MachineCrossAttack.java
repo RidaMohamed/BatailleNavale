9aad8c495d9c90f247cb5b984de73e851a462226
salhi17u <mohamed-elridha.salhi7@etu.univ-lorraine.fr>
2019-12-21 11:00:39 +0100

    Fixing erreur Strategy

--- src/model/player/strategy/MachineCrossAttack.java
+++ src/model/player/strategy/MachineCrossAttack.java
@@ -9,6 +9,7 @@ import model.global.Position;
 import model.global.Turn;
 import model.player.MachinePlayer;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +17,8 @@ public class MachineCrossAttack implements StrategyMahcineAttack {
     private BattleNavaleGame battleNavaleGame;
     private Position x1,x2,x3,x4;
     private boolean randomIsOut=true;
-    private int chosenPos=1;
+    private int chosenPos=0;
+    private boolean next = true;
 
     public MachineCrossAttack(BattleNavaleGame battleNavaleGame) {
         this.battleNavaleGame = battleNavaleGame;
@@ -32,45 +34,61 @@ public class MachineCrossAttack implements StrategyMahcineAttack {
 
         do {
             if(randomIsOut) {
-                randX = (int) (Math.random() * (Constants.WIDTH) + 1);
-                randY = (int) (Math.random() * (Constants.HEIGHT) + 1);
+
                 // Generating a random
                 //System.out.println("la machine a choisi " + randX + "  "+randY);
                 //get borad of humain model.player
                 //Verfie is the postions is ok to attack
+                randX = (int) (Math.random()*(Constants.WIDTH )+1);
+                randY = (int) (Math.random()*(Constants.HEIGHT)+1);
                 b = board.isPosFree(randX, randY);
                 System.out.println(randomIsOut);
             }else{
                 switch(chosenPos){
                     case 1:
-                        if (x1.getX() <= Constants.WIDTH) {
+                        if (x1 != null) {
                             randX = x1.getX();
                             randY = x1.getY();
                             break;
                         }
+                        next = false;
+                        break;
                     case 2 :
-                        if (x2.getY() <= Constants.HEIGHT) {
+                        if (x2 != null) {
                             randX = x2.getX();
                             randY = x2.getY();
                             break;
                         }
+                        next = false;
+                        break;
                     case 3:
-                        if (x3.getX() >= 1) {
+                        if (x3 != null) {
                             randX = x3.getX();
                             randY = x3.getY();
                             break;
                         }
+                        next = false;
+                        break;
                     case 4 :
-                        if (x4.getY() >= 1) {
+                        if (x3 != null) {
                             randX = x4.getX();
                             randY = x4.getY();
                             break;
                         }
+                        next = false;
+                        break;
                 }
-                b = board.isPosFree(randX, randY);
+
+                if (next)
+                    b = board.isPosFree(randX, randY);
+                else {
+                    b = false;
+                    next = true;
+                }
+
                 if (!b){
                     if (this.chosenPos == 4){
-                        this.chosenPos=1;
+                        this.chosenPos=0;
                         randomIsOut=true;
                     }else {
                         chosenPos++;
@@ -80,6 +98,7 @@ public class MachineCrossAttack implements StrategyMahcineAttack {
         } while (!b);
 
         boolean missedShot = true;
+        randomIsOut=false;
         List<Boat> boats = battleNavaleGame.getHumanPlayer().getGame().
                 getHumanPlayer().getBoard().getBoats();
         for (Boat boat : boats) {
@@ -87,18 +106,17 @@ public class MachineCrossAttack implements StrategyMahcineAttack {
             if (boat.isOnCase(randX, randY)) {
                 System.out.println(randX + " / " + randY);
 
-                randomIsOut=false;
-                chosenPos = 1;
                 //adding the hited pos to the borad shoot list
                 board.addPosAttacked(new Position(randX, randY), true);
                 boat.boatIsHit(randX, randY);
                 missedShot = false;
+                chosenPos = 0;
 
-                if(randX+1<=Constants.WIDTH+1){
+                if(randX+1<=Constants.WIDTH){
                     x1=new Position(randX+1,randY);
                     System.out.println(x1.getX() + " /// " + x1.getY());
                 }
-                if(randY+1<=Constants.HEIGHT+1){
+                if(randY+1<=Constants.HEIGHT){
                     x2=new Position(randX,randY+1);
                     System.out.println(x2.getX() + " /// " + x2.getY());
                 }
@@ -126,14 +144,32 @@ public class MachineCrossAttack implements StrategyMahcineAttack {
 
         if (missedShot){
             board.addPosAttacked(new Position(randX, randY), false);
-            if(!randomIsOut){
-                chosenPos++;
-                if(chosenPos==5){
-                    randomIsOut=true;
-                    chosenPos=1;
+            if(chosenPos == 0){
+                if(randX+1<=Constants.WIDTH){
+                    x1=new Position(randX+1,randY);
+                    System.out.println(x1.getX() + " /// " + x1.getY());
+                }
+                if(randY+1<=Constants.HEIGHT){
+                    x2=new Position(randX,randY+1);
+                    System.out.println(x2.getX() + " /// " + x2.getY());
+                }
+                if (randX-1>=1){
+                    x3=new Position(randX-1,randY);
+                    System.out.println(x3.getX() + " /// " + x3.getY());
+                }
+                if (randY-1>=1){
+                    x4=new Position(randX,randY-1);
+                    System.out.println(x4.getX() + " /// " + x4.getY());
                 }
             }
+        }
 
+        if(!randomIsOut){
+            chosenPos++;
+            if(chosenPos==5){
+                randomIsOut=true;
+                chosenPos=0;
+            }
         }
 
         battleNavaleGame.getHumanPlayer().getGame().setTurn(Turn.PlayerTurn);
